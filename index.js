1c0903d5b6a5c759eabac05e83d8a2e3a8735995
Glen <glen.84@gmail.com>
2020-01-24 21:29:33 +0200

    Add "@typescript-eslint/no-non-null-asserted-optional-chain" rule

--- index.js
+++ index.js
@@ -124,6 +124,7 @@ module.exports = {
         "@typescript-eslint/no-misused-new": "error",
         "@typescript-eslint/no-misused-promises": "error",
         "@typescript-eslint/no-namespace": "error",
+        "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
         "@typescript-eslint/no-non-null-assertion": "error",
         "@typescript-eslint/no-parameter-properties": "error",
         "@typescript-eslint/no-require-imports": "error",
