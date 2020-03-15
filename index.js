c355fe5faac08715a3efc51980101342856f7911
Glen <glen.84@gmail.com>
2020-03-15 15:40:16 +0200

    Add "@typescript-eslint/no-unsafe-member-access" rule

--- index.js
+++ index.js
@@ -151,6 +151,7 @@ module.exports = {
         "@typescript-eslint/no-unnecessary-type-arguments": "error",
         "@typescript-eslint/no-unnecessary-type-assertion": "error",
         "@typescript-eslint/no-unsafe-call": "error",
+        "@typescript-eslint/no-unsafe-member-access": "error",
         "@typescript-eslint/no-unused-expressions": [
             "error",
             {allowShortCircuit: true, allowTernary: true}
